d5940db7ff8934cc9df1923381969115297b8d83
Mikhail Kozitskii <kekouke@yandex.ru>
2020-03-16 14:43:27 +1000

    Code refactoring

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -16,213 +16,171 @@ namespace Calculator
             InitializeComponent();
         }
 
-        #region Чтение кнопок
-        private void button_5_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("5");
-        }
+        public CalculatorMachine Calc = new CalculatorMachine();
 
-        private void button_8_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("8");
-        }
+        #region Чтение чисел
 
-        private void button_9_Click(object sender, RoutedEventArgs e)
+        private void button_0_Click(object sender, RoutedEventArgs e)
         {
-            ButtonParse("9");
-        }
-
-        private void button_7_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("7");
-        }
-
-        private void button_4_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("4");
-        }
-
-        private void button_6_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("6");
+            ButtonInt("0");
         }
 
         private void button_1_Click(object sender, RoutedEventArgs e)
         {
-            ButtonParse("1");
-        }
-
-        private void button_mtpl_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("*");
+            ButtonInt("1");
         }
 
         private void button_2_Click(object sender, RoutedEventArgs e)
         {
-            ButtonParse("2");
+            ButtonInt("2");
         }
 
         private void button_3_Click(object sender, RoutedEventArgs e)
         {
-            ButtonParse("3");
+            ButtonInt("3");
         }
 
-        private void button_minus_Click(object sender, RoutedEventArgs e)
+        private void button_4_Click(object sender, RoutedEventArgs e)
         {
-            ButtonParse("-");
+            ButtonInt("4");
         }
 
-        private void button_clear_Click(object sender, RoutedEventArgs e)
+        private void button_5_Click(object sender, RoutedEventArgs e)
         {
-            ButtonParse("C");
+            ButtonInt("5");
         }
 
-        private void button_plus_Click(object sender, RoutedEventArgs e)
+        private void button_6_Click(object sender, RoutedEventArgs e)
         {
-            ButtonParse("+");
+            ButtonInt("6");
         }
 
-        private void button_answer_Click(object sender, RoutedEventArgs e)
+        private void button_7_Click(object sender, RoutedEventArgs e)
         {
-            ButtonParse("=");
+            ButtonInt("7");
         }
 
-        private void button_0_Click(object sender, RoutedEventArgs e)
+        private void button_8_Click(object sender, RoutedEventArgs e)
         {
-            ButtonParse("0");
+            ButtonInt("8");
         }
 
-        private void button_point_Click(object sender, RoutedEventArgs e)
+        private void button_9_Click(object sender, RoutedEventArgs e)
         {
-            ButtonParse(".");
-        }
-
-        private void button_sqrt_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("sqrt");
-        }
-
-
-        private void button_inv_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("1/x");
-        }
-
-        private void button_dl_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("b");
-        }
-
-        private void button_MC_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("MC");
-        }
-
-        private void button_MR_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("MR");
-        }
-
-        private void button_MPlus_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("M+");
-        }
-
-        private void button_MMinus_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("M-");
-        }
-
-        private void button_CE_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("CE");
-        }
-
-        private void button_ChangeSin_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("+/-");
-        }
-
-        private void button_div_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("/");
-        }
-
-        private void button_sqr_Click(object sender, RoutedEventArgs e)
-        {
-            ButtonParse("x^2");
+            ButtonInt("9");
         }
 
         #endregion
 
-        public CalculatorMachine Calc = new CalculatorMachine();
+        #region Чтение кнопок
 
-        private void ButtonParse(string buttonContent)
+        private void button_mtpl_Click(object sender, RoutedEventArgs e)
         {
-            bool isNumber = Int32.TryParse(buttonContent, out _);
+            OperateParse("*");
+        }
 
-            if (isNumber || buttonContent == ",")
+        private void button_minus_Click(object sender, RoutedEventArgs e)
+        {
+            OperateParse("-");
+        }
+
+        private void button_clear_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = Calc.Clear();
+            historyBlock.Text = String.Empty;
+        }
+
+        private void button_plus_Click(object sender, RoutedEventArgs e)
+        {
+            OperateParse("+");
+        }
+
+        private void button_answer_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = Calc.Equal();
+            historyBlock.Text = String.Empty;
+        }
+
+        private void button_point_Click(object sender, RoutedEventArgs e)
+        {
+            if (Calc.operation == "")
             {
-                if (Calc.operation == "")
-                {
-                    if (Calc.isAnswer)
-                    {
-                        Calc.leftOperand = "0";
-                        Calc.isAnswer = false;
-                        historyBlock.Text = "";
-                    }
-
-                    NumberClick(ref Calc.leftOperand, buttonContent);
-                }
-                else
-                {
-                    NumberClick(ref Calc.rightOperand, buttonContent);
-                }
+                textBlock.Text = Calc.PointParse(ref Calc.leftOperand);
             }
             else
             {
-                if (buttonContent == "=")
-                {
-                    Equal();
-                }
-                else if (buttonContent == "C")
-                {
-                    Clear();
-                }
-                else if (buttonContent == "+/-")
-                {
-                    ChangeSign();
-                }
-                else if (buttonContent == "CE")
-                {
-                    ClearWindow();
-                }
-                else if (buttonContent == "b")
-                {
-                    ClearLastSymbol();
-                }
-                else if (buttonContent == "sqrt")
-                {
-                    Sqrt();
-                } 
-                else if (buttonContent == "x^2")
-                {
-                    Square();
-                }
-                else if (buttonContent == "1/x")
-                {
-                    Inverse();
-                }
-                else if (buttonContent.Contains("M"))
-                {
-                    MemoryOperate(buttonContent);
-                }
-                else
-                {
-                    Operate(buttonContent);
-                }
+                textBlock.Text = Calc.PointParse(ref Calc.rightOperand);
+            }
+        }
+
+        private void button_dl_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = Calc.ClearLastSymbol();
+        }
+
+        private void button_MS_Click(object sender, RoutedEventArgs e)
+        {
+            var memoryData = textBlock.Text;
+            memoryBlock.Text = Calc.MemoryOperate("MS", memoryData); 
+        }
+
+        private void button_MC_Click(object sender, RoutedEventArgs e)
+        {
+            var memoryData = textBlock.Text;
+            memoryBlock.Text = Calc.MemoryOperate("MC", memoryData);
+        }
+
+        private void button_MR_Click(object sender, RoutedEventArgs e)
+        {
+            var memoryData = textBlock.Text;
+            textBlock.Text = Calc.MemoryOperate("MR", memoryData);
+        }
+
+        private void button_MPlus_Click(object sender, RoutedEventArgs e)
+        {
+            var memoryData = textBlock.Text;
+            memoryBlock.Text = Calc.MemoryOperate("M+", memoryData);
+        }
+
+        private void button_MMinus_Click(object sender, RoutedEventArgs e)
+        {
+            var memoryData = textBlock.Text;
+            memoryBlock.Text = Calc.MemoryOperate("M-", memoryData);
+        }
+
+        private void button_CE_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = Calc.ClearWindow();
+        }
+
+        private void button_ChangeSin_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = Calc.ChangeSign();
+        }
+
+        private void button_div_Click(object sender, RoutedEventArgs e)
+        {
+            OperateParse("/");
+        }
+
+        #endregion
+
+        private void OperateParse(string buttonContent)
+        {
+            textBlock.Text = Calc.Operate(buttonContent);
+            historyBlock.Text = Calc.operation;
+        }     
+
+        private void ButtonInt(string buttonContent)
+        {
+
+            if (Calc.isAnswer && Calc.operation == String.Empty)
+            {
+                Calc.leftOperand = "0";
+                Calc.isAnswer = false;
             }
 
+            textBlock.Text = Calc.NumberClick(buttonContent);
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -236,203 +194,5 @@ namespace Calculator
                 MessageBox.Show("Калькулятор 1.0.0.0\nСпособен выполнять базовые арифметические операции" +
                             " - сложение, вычитание, умножение, деление.\n© Козицкий Михаил(kekouke),\n2020.");
         }
-
-        private string PointParse(ref string operand, string content)
-        {
-
-            if (content == ",")
-            {
-
-                if (operand == ",")
-                {
-                    operand = "0,";
-                }
-                else if (operand[0..^1].Contains(","))
-                {
-                    operand = operand[0..^1];
-                }
-
-                return operand;
-            }
-            return null;
-        }
-
-        private void Equal()
-        {
-            if (Calc.rightOperand == "")
-            {
-                textBlock.Text = Calc.leftOperand;
-            }
-            else
-            {
-                historyBlock.Text += Calc.rightOperand;
-                textBlock.Text = Calc.Calc();
-            }
-           // historyBlock.Text += Calc.rightOperand;
-            Calc.operation = "";
-        }
-
-        private void Clear()
-        {
-            Calc.leftOperand = textBlock.Text = "0";
-            Calc.rightOperand = "0";
-            Calc.operation = "";
-            historyBlock.Text = "";
-        }
-
-        private void ClearWindow()
-        {
-            if (Calc.operation == "")
-            {
-                textBlock.Text = Calc.leftOperand = "0";
-                historyBlock.Text = "";
-            }
-            else
-            {
-                textBlock.Text = Calc.rightOperand = "0";
-            }
-        }
-
-        private void ClearLastSymbol()
-        {
-            if (!Calc.isAnswer)
-            {
-                if (Calc.operation == "")
-                {
-                    ClearLastSymbol(ref Calc.leftOperand);
-                }
-                else
-                {
-                    ClearLastSymbol(ref Calc.rightOperand);
-                }
-            }
-        }
-
-        private void ClearLastSymbol(ref string value)
-        {
-                if (value.Length == 1 && value != "0")
-                {
-                    textBlock.Text = value = "0";
-                }
-                else if (value.Length > 1)
-                {
-                    textBlock.Text = value = value[0..^1];
-                }
-        }
-
-        private void Operate(string buttonContent)
-        {
-            if (Calc.rightOperand != "0")
-            {
-                Equal();
-            }
-
-            if (!textBlock.Text.Contains("Can't divide by zero"))
-            {
-                textBlock.Text = Calc.leftOperand;// + buttonContent;
-                Calc.operation = buttonContent;
-            }
-
-            historyBlock.Text = Calc.leftOperand + Calc.operation;
-
-        }
-
-        private void NumberClick(ref string operand, string buttonContent)
-        {
-            if (operand.Length < 16)
-            {
-                if (operand == "0")
-                {
-                    operand = buttonContent;
-                }
-                else
-                {
-                    operand += buttonContent;
-                }
-                textBlock.Text = operand;
-                textBlock.Text = PointParse(ref operand, buttonContent) ?? textBlock.Text;
-            }
-        }     
-
-        private void ChangeSign()
-        {
-            if (Calc.operation == "")
-            {
-                textBlock.Text = Calc.leftOperand = (-1 * double.Parse(Calc.leftOperand)).ToString();
-            }
-            else
-            {
-                textBlock.Text = Calc.rightOperand = (-1 * double.Parse(Calc.rightOperand)).ToString();
-            }
-        }
-
-        private void Sqrt()
-        {
-            if (double.Parse(Calc.leftOperand) < 0 || double.Parse(Calc.rightOperand) < 0)
-            {
-                Clear();
-                textBlock.Text = "Error";
-                return;
-            }
-
-            if (Calc.operation == "")
-            {
-                textBlock.Text = Calc.leftOperand = (Math.Sqrt(double.Parse(Calc.leftOperand))).ToString();
-            }
-            else
-            {
-                textBlock.Text = Calc.rightOperand = (Math.Sqrt(double.Parse(Calc.rightOperand))).ToString();
-            }
-        }
-
-        private void Square()
-        {
-            if (Calc.operation == "")
-            {
-                textBlock.Text = Calc.leftOperand = (double.Parse(Calc.leftOperand) * double.Parse(Calc.leftOperand)).ToString();
-            }
-            else
-            {
-                textBlock.Text = Calc.rightOperand = (double.Parse(Calc.rightOperand) * double.Parse(Calc.rightOperand)).ToString();
-            }
-        }
-
-        private void Inverse()
-        {
-            Calc.leftOperand = "1";
-            Calc.rightOperand = textBlock.Text;
-            Calc.operation = "/";
-            Equal();
-        }
-
-        private void MemoryOperate(string buttonContent)
-        {
-            if (buttonContent == "MC")
-            {
-                Calc.memoryActive = false;
-                Calc.memory = "0";
-                memoryBlock.Text = "В памяти ничего не сохранено";
-            }
-            else if (buttonContent == "MR")
-            {
-                if (Calc.memoryActive)
-                {
-                    textBlock.Text = Calc.leftOperand = Calc.memory;
-                }
-            }
-            else if (buttonContent == "M+")
-            {
-                Calc.memory = (double.Parse(Calc.memory) + double.Parse(textBlock.Text)).ToString();
-                Calc.memoryActive = true;
-                memoryBlock.Text = Calc.memory;
-            }
-            else if (buttonContent == "M-")
-            {
-                Calc.memory = (double.Parse(Calc.memory) - double.Parse(textBlock.Text)).ToString();
-                Calc.memoryActive = true;
-                memoryBlock.Text = Calc.memory;
-            }
-        }
-
     }
 }
--- Calculator/Model/Calculator.cs
+++ Calculator/Model/Calculator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
-
+using System.Windows;
 namespace Calculator.Model
 {
     public class CalculatorMachine
@@ -13,15 +12,17 @@ namespace Calculator.Model
         public string rightOperand;
         public string operation;
         public string memory;
+        private string result;
         public bool isAnswer = false;
         public bool memoryActive = false;
 
         public CalculatorMachine()
         {
             leftOperand = "0";
-            rightOperand = "0";
+            rightOperand = "";
             operation = "";
             memory = "0";
+            result = "^";
         }
 
         public string Calc()
@@ -30,7 +31,9 @@ namespace Calculator.Model
             num1 = double.Parse(leftOperand);
 
             if (rightOperand != "")
+            {
                 num2 = double.Parse(rightOperand);
+            }
 
             rightOperand = "";
 
@@ -52,6 +55,203 @@ namespace Calculator.Model
                 isAnswer = true;
                 return leftOperand = Math.Round((_operations[operation](num1, num2)), 10).ToString();
             }
+        }
+
+        public string Equal()
+        {
+            if (operation == String.Empty)
+            {
+                return leftOperand;
+
+            }
+            else
+            {
+                result = Calc();
+            }
+            operation = String.Empty; // Возможно, это можно удалить в функции Calc()
+            return result;
+        }
+
+        public string Clear()
+        {
+            leftOperand = "0";
+            rightOperand = "";
+            operation = "";
+            return "0";
+        }
+
+        public string ClearWindow()
+        {
+            if (operation == String.Empty)
+            {
+                leftOperand = "0";
+            }
+            else
+            {
+                rightOperand = "";
+            }
+            return "0";
+        }
+
+        public string ClearLastSymbol()
+        {
+            string value;
+            if (!isAnswer)
+            {
+                if (operation == String.Empty)
+                {
+                    value = ClearLastSymbol(ref leftOperand);
+                }
+                else
+                {
+                    value = ClearLastSymbol(ref rightOperand);
+                }
+                return value;
+            }
+            else
+            {
+                return leftOperand;
+            }
+            
+        }
+
+        private string ClearLastSymbol(ref string value)
+        {
+            if (value.Length == 1 && value != "0")
+            {
+                value = "0";
+            }
+            else if (value.Length > 1)
+            {
+                value = value[0..^1];
+            }
+
+            return value;
+        }
+
+        public string Operate(string buttonContent)
+        {
+            result = "^";
+
+            if (rightOperand != "")
+            {
+                result = Equal();
+            }
+
+            if (!result.Contains("Can't divide by zero"))
+            {
+                result = leftOperand;
+                operation = buttonContent;
+            }
+            return result;
+        }
+
+        public string NumberClick(string buttonContent) // TODO : ENUM
+        {
+            if (operation == "")
+            {
+                if (leftOperand.Length < 13)
+                {
+                    if (leftOperand == "0")
+                    {
+                        leftOperand = buttonContent;
+                    }
+                    else
+                    {
+                        leftOperand += buttonContent;
+                    }
+                }
+                return leftOperand;
+            }
+            else
+            {
+                if (rightOperand.Length < 13)
+                {
+
+                    if (rightOperand == "" || rightOperand == "0")
+                    {
+                        rightOperand = buttonContent;
+                    }
+                    else
+                    {
+                        rightOperand += buttonContent;
+                    }
+                }
+                return rightOperand;
+            }
+
+            
+        }
+
+        public string ChangeSign()
+        {
+            if (operation == "")
+            {
+                result = leftOperand = (-1 * double.Parse(leftOperand) + 0).ToString();
+            }
+            else
+            {
+                if (rightOperand == String.Empty)
+                {
+                    rightOperand = "0";
+                }
+                result = rightOperand = (-1 * double.Parse(rightOperand) + 0).ToString();
+            }
+
+            return result;
+        }
+
+        public string MemoryOperate(string buttonContent, string memoryData)
+        {
+            if (buttonContent == "MC")
+            {
+                memoryActive = false;
+                memory = "0";
+                return "В памяти ничего не сохранено";
+            }
+            else if (buttonContent == "MR")
+            {
+                if (memoryActive)
+                {
+                    leftOperand = memory;
+                    return memory;
+                }
+            }
+            else if (buttonContent == "M+")
+            {
+                memory = (double.Parse(memory) + double.Parse(memoryData)).ToString();
+                memoryActive = true;
+                return memory;
+            }
+            else if (buttonContent == "M-")
+            {
+                memory = (double.Parse(memory) - double.Parse(memoryData)).ToString();
+                memoryActive = true;
+                return memory;
+            }
+            else if (buttonContent == "MS")
+            {
+                memory = memoryData;
+                memoryActive = true;
+                return memory;
+            }
+            return memoryData;
+
+        }
+
+        public string PointParse(ref string operand)
+        {          
+            if (!operand.Contains(","))
+            {
+                operand += ",";
+            }
+
+            if (operand == ",")
+            {
+                operand = "0,";
+            }
+
+            return operand;
         }
     }
 }
